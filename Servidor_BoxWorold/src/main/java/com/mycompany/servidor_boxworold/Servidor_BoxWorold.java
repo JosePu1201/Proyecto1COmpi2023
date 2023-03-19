61a76a8a7f35e5295e765caab7d85757e635e5e5
JosePu1201 <joseluis-pusam@cunoc.edu.gt>
2023-03-18 23:53:01 -0600

    ejecutables

--- Servidor_BoxWorold/src/main/java/com/mycompany/servidor_boxworold/Servidor_BoxWorold.java
+++ Servidor_BoxWorold/src/main/java/com/mycompany/servidor_boxworold/Servidor_BoxWorold.java
@@ -25,7 +25,6 @@ import javax.swing.JFrame;
 public class Servidor_BoxWorold {
 
     public static void main(String[] args) {
-
         Toolkit pantalla = Toolkit.getDefaultToolkit();
         Dimension tPantalla = pantalla.getScreenSize(); // guarda el tamaño de la pantalla
         int altura = tPantalla.height;
@@ -39,39 +38,6 @@ public class Servidor_BoxWorold {
         ventana.getContentPane().add(pedir);
         ventana.setVisible(true);
         
-//        entrada nuevo = new entrada(9090);
-//        nuevo.start();
-//		Scanner entrada = null;
-//		JFileChooser fileChooser = new JFileChooser();
-//		fileChooser.showOpenDialog(fileChooser);
-//		String algo = "";
-//		try {
-//			String ruta = fileChooser.getSelectedFile().getAbsolutePath();
-//			File f = new File(ruta);
-//			entrada = new Scanner(f);
-//			while (entrada.hasNext()) {
-//				algo = algo  + entrada.nextLine();
-//			}
-//		} catch (FileNotFoundException e) {
-//			System.out.println(e.getMessage());
-//		} catch (NullPointerException e) {
-//			System.out.println("No se ha seleccionado ningún fichero");
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//		} finally {
-//			if (entrada != null) {
-//				entrada.close();
-//			}
-//		}
-//
-//		Reader adentro = new StringReader(algo);
-//                Lexer nuevo = new Lexer(adentro);
-//                parser nuevo1 = new parser(nuevo);
-//                try {
-//                    nuevo1.parse();
-//                
-//            } catch (Exception e) {
-//                    System.out.println("no sale xD");
-//            }
+
     }
 }
